7a183f10157581aeef943c6ed42a900e22cb08ec
Roger Wang <rogerwang2520@gmail.com>
2023-05-12 17:46:25 -0500

    Tweak launch velocity parameters

--- Assets/Project/Scripts/LaunchPlayer.cs
+++ Assets/Project/Scripts/LaunchPlayer.cs
@@ -6,6 +6,7 @@ public class LaunchPlayer : MonoBehaviour
 {
     [SerializeField] float launchForceMultiplier = 1f;
     [SerializeField] float distanceAtMaxForce = 1f;  // Maximum force is applied at this distance and closer
+    [Range(0.1f, 10f)] [SerializeField] float forceDistanceInverseExponent = 2f;
     [SerializeField] float mass = 1f;
 
     Rigidbody rb;
@@ -23,7 +24,7 @@ public class LaunchPlayer : MonoBehaviour
         Vector3 launchDirection = (transform.position - launchPosOrigin).normalized;
         float distance = Vector3.Distance(launchPosOrigin, transform.position);  // Could use sqrMagnitude instead
         distance = Mathf.Max(distance, distanceAtMaxForce);
-        float forceMagnitude = launchForceMultiplier / (distance * distance);
+        float forceMagnitude = launchForceMultiplier / Mathf.Pow(distance, forceDistanceInverseExponent);
         Vector3 force = (transform.position - launchPosOrigin) * forceMagnitude;
         Vector3 velocity = force / mass;
         Debug.Log($"launchHit: {launchPosOrigin} | transform.position: {transform.position} | launchDirection: {launchDirection} | distance: {distance} | launchSpeed: {velocity.magnitude}");
