26a8916fda0a695763f2dea463d17621c449ad69
Roger Wang <rogerwang2520@gmail.com>
2023-05-14 18:09:29 -0500

    Tweak launch velocity parameters 2

--- Assets/Project/Scripts/LaunchPlayer.cs
+++ Assets/Project/Scripts/LaunchPlayer.cs
@@ -6,7 +6,7 @@ public class LaunchPlayer : MonoBehaviour
 {
     [SerializeField] float launchForceMultiplier = 1f;
     [SerializeField] float distanceAtMaxForce = 1f;  // Maximum force is applied at this distance and closer
-    [Range(0.1f, 10f)] [SerializeField] float forceDistanceInverseExponent = 2f;
+    [Range(0f, 10f)] [SerializeField] float forceDistanceInverseExponent = 2f;
     [SerializeField] float mass = 1f;
 
     Rigidbody rb;
